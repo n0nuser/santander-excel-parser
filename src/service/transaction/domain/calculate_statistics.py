6fa5b12db0665314f1c91205b117747add3bd46d
n0nuser <gonzrubio.pablo@gmail.com>
2024-06-02 18:33:41 +0200

    ♻️ Refactored statistics into functions

--- src/service/transaction/domain/calculate_statistics.py
+++ src/service/transaction/domain/calculate_statistics.py
@@ -12,16 +12,15 @@ from src.controller.api.schemas.transactions.statistics import (
 )
 
 
-def calculate_statistics(transactions_df: DataFrame) -> BankTransactionStatistics:
-    """Calculate statistics from a DataFrame of transactions.
+def basic_statistics(transactions_df: DataFrame) -> BasicStatistics:
+    """Calculate basic statistics from a DataFrame of transactions.
 
     Args:
         transactions_df (DataFrame): DataFrame of transactions.
 
     Returns:
-        BankTransactionStatistics: Statistics of the transactions.
+        BasicStatistics: Basic statistics of the transactions.
     """
-    # Basic Statistics
     total_transactions = len(transactions_df)
     total_deposited = transactions_df[transactions_df["amount"] > 0]["amount"].sum()
     total_withdrawn = transactions_df[transactions_df["amount"] < 0]["amount"].sum()
@@ -46,7 +45,7 @@ def calculate_statistics(transactions_df: DataFrame) -> BankTransactionStatistic
         for key, value in transactions_per_concept_dict.items()
     ]
 
-    basic_statistics = BasicStatistics(
+    return BasicStatistics(
         total_transactions=total_transactions,
         total_deposited=total_deposited,
         total_withdrawn=total_withdrawn,
@@ -55,7 +54,31 @@ def calculate_statistics(transactions_df: DataFrame) -> BankTransactionStatistic
         transactions_per_concept=transactions_per_concept_model,
     )
 
-    # Time-Based Analysis
+
+def account_based_analysis(transactions_df: DataFrame) -> AccountBasedAnalysis:
+    """Calculate account-based analysis from a DataFrame of transactions.
+
+    Args:
+        transactions_df (DataFrame): DataFrame of transactions.
+
+    Returns:
+        AccountBasedAnalysis: Account-based analysis of the transactions.
+    """
+    return AccountBasedAnalysis(
+        average_balance=transactions_df["balance"].mean(),
+        final_balance=transactions_df["balance"].iloc[-1],
+    )
+
+
+def time_based_analysis(transactions_df: DataFrame) -> TimeBasedAnalysis:
+    """Calculate time-based analysis from a DataFrame of transactions.
+
+    Args:
+        transactions_df (DataFrame): DataFrame of transactions.
+
+    Returns:
+        TimeBasedAnalysis: Time-based analysis of the transactions.
+    """
     transactions_df["operation_original_date"] = pd.to_datetime(
         transactions_df["operation_original_date"]
     )
@@ -71,24 +94,21 @@ def calculate_statistics(transactions_df: DataFrame) -> BankTransactionStatistic
         .to_dict()
     )
 
-    time_based_analysis = TimeBasedAnalysis(
+    return TimeBasedAnalysis(
         daily_transactions=daily_transactions,
         monthly_transactions={str(k): v for k, v in monthly_transactions.items()},
     )
 
-    account_based_analysis = AccountBasedAnalysis(
-        average_balance=transactions_df["balance"].mean(),
-        final_balance=transactions_df["balance"].iloc[-1],
-    )
 
-    # Aggregating transactions by account and date to ensure unique combinations
-    aggregated_df = (
-        transactions_df.groupby(["account_id", "operation_effective_date"])
-        .agg({"amount": "sum", "balance": "last"})
-        .reset_index()
-    )
+def advanced_insights(transactions_df: DataFrame) -> AdvancedInsights:
+    """Calculate advanced insights from a DataFrame of transactions.
 
-    # Advanced Insights
+    Args:
+        transactions_df (DataFrame): DataFrame of transactions.
+
+    Returns:
+        AdvancedInsights: Advanced insights of the transactions.
+    """
     largest_deposit = transactions_df[transactions_df["amount"] > 0].nlargest(1, "amount").iloc[0]
     largest_withdrawal = (
         transactions_df[transactions_df["amount"] < 0].nsmallest(1, "amount").iloc[0]
@@ -110,6 +130,12 @@ def calculate_statistics(transactions_df: DataFrame) -> BankTransactionStatistic
         balance=largest_withdrawal["balance"],
     )
 
+    # Aggregating transactions by account and date to ensure unique combinations
+    aggregated_df = (
+        transactions_df.groupby(["account_id", "operation_effective_date"])
+        .agg({"amount": "sum", "balance": "last"})
+        .reset_index()
+    )
     daily_ending_balance_df = (
         aggregated_df.set_index("operation_effective_date")
         .groupby("account_id")["balance"]
@@ -121,16 +147,30 @@ def calculate_statistics(transactions_df: DataFrame) -> BankTransactionStatistic
         index="operation_effective_date", columns="account_id", values="balance"
     ).to_dict()
 
-    advanced_insights = AdvancedInsights(
+    return AdvancedInsights(
         largest_deposit=largest_deposit_model,
         largest_withdrawal=largest_withdrawal_model,
         daily_ending_balance=daily_ending_balance_dict,
     )
 
+
+def calculate_statistics(transactions_df: DataFrame) -> BankTransactionStatistics:
+    """Calculate statistics from a DataFrame of transactions.
+
+    Args:
+        transactions_df (DataFrame): DataFrame of transactions.
+
+    Returns:
+        BankTransactionStatistics: Statistics of the transactions.
+    """
+    basic_statistics_model = basic_statistics(transactions_df)
+    time_based_analysis_model = time_based_analysis(transactions_df)
+    account_based_analysis_model = account_based_analysis(transactions_df)
+    advanced_insights_model = advanced_insights(transactions_df)
     # Consolidate all statistics into one model
     return BankTransactionStatistics(
-        basic_statistics=basic_statistics,
-        time_based_analysis=time_based_analysis,
-        account_based_analysis=account_based_analysis,
-        advanced_insights=advanced_insights,
+        basic_statistics=basic_statistics_model,
+        time_based_analysis=time_based_analysis_model,
+        account_based_analysis=account_based_analysis_model,
+        advanced_insights=advanced_insights_model,
     )
